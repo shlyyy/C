054a168676484dcdcb456d47aa34aec9fdf125ca
shlyyy <shlyyy@foxmail.com>
2022-03-20 16:06:44 +0800

    素数

--- C_Code/test_22_3_20/PrimeNumber.c
+++ C_Code/test_22_3_20/PrimeNumber.c
@@ -6,13 +6,14 @@
 /*
 打印100~200之间的素数
 */
+// 试除法
 void PrimeNumber()
 {
     int i = 0;
     int j = 0;
     bool is_prime = true;
 
-    for (i = 100; i <= 200; i++)
+    for (i = 101; i <= 200; i+=2)
     {
         is_prime = true;
         for (j = 2; j <= sqrt(i); j++)
